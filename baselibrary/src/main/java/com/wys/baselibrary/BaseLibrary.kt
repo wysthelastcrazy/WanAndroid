60908d3bc4b7a82226c667092859781c3b9bf8bb
王亚胜 <wangyasheng@gaosiedu.com>
2020-07-24 15:10:05 +0800

    base

---
+++ baselibrary/src/main/java/com/wys/baselibrary/BaseLibrary.kt
@@ -0,0 +1,14 @@
+package com.wys.baselibrary
+
+/**
+ * @author wangyasheng
+ * @date 2020/7/24
+ * @Describe:
+ */
+class BaseLibrary {
+    companion object{
+        fun initRequest(){
+
+        }
+    }
+}
